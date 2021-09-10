a5564dff148f32e116c6d6804541e913540988b1
Flávio Arêas <vflavio9@gmail.com>
2021-09-09 22:39:50 -0300

    feat: create UnForm Input component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+	display: flex;
+	flex-direction: column;
+
+	input {
+		flex: 1;
+		width: 100%;
+
+		border: 0;
+		border-radius: 0.25rem;
+		padding: 1rem 0.5rem;
+
+		margin: 0.5rem 0;
+	}
+`;
